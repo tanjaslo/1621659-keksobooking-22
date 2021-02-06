6615c0e98d11ebee914d042d354bca3e952d3a8c
tanjaslo <tanja.slo@ukr.net>
2021-02-06 15:07:44 +0100

    const added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,48 @@
 'use strict';
 
-// Функция, возвращающая случайное целое число из переданного диапазона включительно.
+const OFFER_TITLES = [
+  'Private room in guesthouse Soul',
+  'Riverbank apartment',
+  'Villa Sakura',
+  'Chalet Hidden gem',
+];
 
+const OFFER_DESCRIPTIONS = [
+  'Clean and cosy private room in the center of Tokyo. Accommodation has a dining area, a flat-screen TV and a private bathroom with shower and a hairdryer.',
+  'This accommodation is situated on Gyotoku Riverbank in 300 metres from all local attractions. The apartment offers a children\'s playground. Fishing and hiking can be enjoyed nearby.',
+  'In the heart of Tokyo, set within a short distance of Ruins of Takanawa Okido Gate and Shinsei Temple, this property is a renovated Japanese townhouse fitted with air conditioning and free WiFi. Guests can also relax in the shared lounge area. Free private parking is available.',
+  'Suitable location in an area where guests can engage in activities such as hiking and cycling. This air-conditioned property is composed of 2 separate bedrooms, a living room, a fully equipped kitchen with a microwave and fridge, and 1 bathroom.',
+];
+
+const HOUSING_TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+
+const CHECK_TIME = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+// Функция, возвращающая случайное целое число из переданного диапазона включительно.
 const getRandomIntInclusive = (min, max) => {
   if (min >= max || min < 0 || max < 0) {
     return null;
@@ -11,7 +52,6 @@ const getRandomIntInclusive = (min, max) => {
 getRandomIntInclusive(5, 10);
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-
 const getRandomFloatInclusive = (min, max, numbersAfterPoint) => {
   if (min >= max || min < 0 || max < 0) {
     return null;
