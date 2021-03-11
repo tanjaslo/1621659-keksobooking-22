2ffa0d6d2606d2cddf4807a7d5b793ee55ae3355
tanjaslo <tanja.slo@ukr.net>
2021-03-11 01:17:12 +0100

    Добавляет загрузку аватарки и фотографии жилья.

---
+++ js/photo.js
@@ -0,0 +1,42 @@
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+const DEFAULT_AVATAR_SRC = 'img/muffin-grey.svg';
+const PREVIEW_SIZE = 70;
+
+const avatarUploader = document.querySelector('.ad-form__field input[type=file]');
+const avatarPreview = document.querySelector('.ad-form-header__preview img');
+const housingPhotoUploader = document.querySelector('.ad-form__upload input[type=file]');
+const housingPhotoForm = document.querySelector('.ad-form__photo');
+const housingPhotoPreview = document.createElement('img');
+
+const handleFiles = (fileChooser, preview) => {
+  return (evt) => {
+    evt.preventDefault();
+    const file = fileChooser.files[0];
+    const fileName = file.name.toLowerCase();
+    const matches = FILE_TYPES.some((it) => {
+      return fileName.endsWith(it);
+    });
+    if (matches) {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        preview.src = reader.result;
+      });
+      reader.readAsDataURL(file);
+    }
+  };
+};
+
+const initPhotoAploaders = () => {
+  avatarUploader.addEventListener('change', handleFiles(avatarUploader, avatarPreview));
+  housingPhotoUploader.addEventListener('change', handleFiles(housingPhotoUploader, housingPhotoPreview));
+  housingPhotoPreview.height = PREVIEW_SIZE;
+  housingPhotoPreview.width = PREVIEW_SIZE;
+  housingPhotoForm.append(housingPhotoPreview);
+};
+
+const clearPreview = () => {
+  avatarPreview.src = DEFAULT_AVATAR_SRC;
+  housingPhotoPreview.src = '';
+}
+
+export { initPhotoAploaders, clearPreview }
