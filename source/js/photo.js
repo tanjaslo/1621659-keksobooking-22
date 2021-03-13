d2a95b01be79d82b3ef75d16bc3462286ddb1b18
tanjaslo <tanja.slo@ukr.net>
2021-03-13 11:40:15 +0100

    Добавляет изменения по комментариям проверяющего наставника

--- source/js/photo.js
+++ source/js/photo.js
@@ -1,5 +1,6 @@
 const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
-const DEFAULT_SRC = 'img/muffin-grey.svg';
+const DEFAULT_AVATAR_SRC = 'img/muffin-grey.svg';
+const DEFAULT_HOUSING_SRC = 'img/nothing.svg';
 const PREVIEW_SIZE = 70;
 
 const avatarUploader = document.querySelector('#avatar');
@@ -8,7 +9,7 @@ const housingPhotoUploader = document.querySelector('#images');
 const housingPhotoContainer = document.querySelector('.ad-form__photo');
 const housingPhotoPreview = document.createElement('img');
 
-const handleFiles = (fileChooser, preview) => {
+const onUploaderChange = (fileChooser, preview) => {
   return (evt) => {
     evt.preventDefault();
     const file = fileChooser.files[0];
@@ -27,16 +28,16 @@ const handleFiles = (fileChooser, preview) => {
 };
 
 const initImageUploaders = () => {
-  avatarUploader.addEventListener('change', handleFiles(avatarUploader, avatarPreview));
-  housingPhotoUploader.addEventListener('change', handleFiles(housingPhotoUploader, housingPhotoPreview));
+  avatarUploader.addEventListener('change', onUploaderChange(avatarUploader, avatarPreview));
+  housingPhotoUploader.addEventListener('change', onUploaderChange(housingPhotoUploader, housingPhotoPreview));
   housingPhotoPreview.height = PREVIEW_SIZE;
   housingPhotoPreview.width = PREVIEW_SIZE;
   housingPhotoContainer.append(housingPhotoPreview);
 };
 
 const clearPreview = () => {
-  avatarPreview.src = DEFAULT_SRC;
-  housingPhotoPreview.src = '';
+  avatarPreview.src = DEFAULT_AVATAR_SRC;
+  housingPhotoPreview.src = DEFAULT_HOUSING_SRC;
 };
 
 export { initImageUploaders, clearPreview }
